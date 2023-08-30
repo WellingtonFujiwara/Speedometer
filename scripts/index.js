f84669aa9281b26de1798b3d0efd24bd2812165a
Wellington Fujiwara <w.fujiwara93@gmail.com>
2023-08-29 22:09:07 -0300

    add cidade e velocidade na lista

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,49 @@
 const ridelistElement = document.querySelector('#rideList')
 const allRides = getAllRides()
 
-allRides.forEach(([id, value])=> {
+allRides.forEach(async ([id, value])=> {
     const ride = JSON.parse(value)
     ride.id = id
     const firstPosition = ride.data[0]
-    console.log(firstPosition)
+    /* console.log(firstPosition) */
+    
+    const location = await getLocationData(firstPosition.latitude, firstPosition.longitude)
+
+   
 
     const itemElement = document.createElement('li')
     itemElement.id = ride.id
-    /* itemElement.innerText =  */
+    
+    
+    const cityDiv = document.createElement('div')
+    cityDiv.innerText = `${location.city} - ${location.countryCode}`
+
+    const maxSpeedDiv = document.createElement('div')
+    maxSpeedDiv.innerText = getMaxSpeed(ride.data)
+    
+    
+    
     ridelistElement.appendChild(itemElement)
-
-
-
-    console.log(ride)
+    itemElement.appendChild(cityDiv)
+    itemElement.appendChild(maxSpeedDiv)
+    
 })
+
+async function getLocationData(latitude, longitude) {
+    const urlApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&=localityLanguage=en`
+
+    const response = await fetch(urlApi)
+    return await response.json()
+}
+
+function getMaxSpeed(positions) {
+    let maxSpeed = 0
+    positions.forEach(position=>{
+        if(position.speed != null && position.speed > maxSpeed) {
+            maxSpeed = position.speed
+        }
+    })
+    
+    return maxSpeed
+}
 
